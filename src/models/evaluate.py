7bccd71e846bb9328ad259ca35ec6ff1f0306790
Ubuntu <ubuntu@ip-172-31-0-207.eu-west-1.compute.internal>
2025-03-19 17:57:05 +0000

    delete test register model

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -4,11 +4,6 @@ import pandas as pd
 from sklearn.metrics import mean_squared_error, r2_score
 import joblib
 
-import mlflow
-import mlflow.sklearn
-from mlflow.models import infer_signature
-
-
 
 # Read file
 def read_data(file_path):
@@ -18,51 +13,33 @@ def read_data(file_path):
 
 def evaluate(my_model, X_test_scaled, y_test ,output_folder, output_metrics) : 
     
-    with mlflow.start_run() as run:
+    prediction = my_model.predict(X_test_scaled)
+        
+    # save prediction to prediction.csv and track it with DVC
+    test = pd.DataFrame(prediction, columns=["Prediction"]).to_csv(output_folder + "/prediction.csv", index=False)
 
+
+    # Evaluation
+        # MSE (Mean Squared Error) : Erreur quadratique moyenne => plus c'est bas, mieux c'est.
+        # RMSE (Root Mean Squared Error) : Racine carrée du MSE => permet d'interpréter directement l'erreur en unités de la variable cible.
+        # R² (Coefficient de détermination) : Score entre 0 et 1 qui indique la proportion de la variance expliquée par le modèle (1 étant parfait).
     
-        prediction = my_model.predict(X_test_scaled)
-        
-        # save prediction to prediction.csv and track it with DVC
-        test = pd.DataFrame(prediction, columns=["Prediction"]).to_csv(output_folder + "/prediction.csv", index=False)
+    mse = mean_squared_error(y_test, prediction)
+    rmse = mean_squared_error(y_test, prediction, squared=False)  # RMSE = sqrt(MSE)
+    r2 = r2_score(y_test, prediction)
 
-        # Evaluation
-            # MSE (Mean Squared Error) : Erreur quadratique moyenne => plus c'est bas, mieux c'est.
-            # RMSE (Root Mean Squared Error) : Racine carrée du MSE => permet d'interpréter directement l'erreur en unités de la variable cible.
-            # R² (Coefficient de détermination) : Score entre 0 et 1 qui indique la proportion de la variance expliquée par le modèle (1 étant parfait).
-        
-        mse = mean_squared_error(y_test, prediction)
-        rmse = mean_squared_error(y_test, prediction, squared=False)  # RMSE = sqrt(MSE)
-        r2 = r2_score(y_test, prediction)
+    # Stocker les résultats dans un dictionnaire
+    metrics = {
+        "Mean Squared Error (MSE)": mse,
+        "Root Mean Squared Error (RMSE)": rmse,
+        "R² Score": r2
+    }
+    
+    print (metrics)
 
-        # Stocker les résultats dans un dictionnaire
-        metrics = {
-            "Mean_Squared_Error": mse,
-            "Root_Mean_Square_Error": rmse,
-            "R_carre_Score": r2
-        }
-        
-        print (metrics)
-
-        # Sauvegarder les métriques dans un fichier JSON
-        with open(output_metrics + "/scores.json", "w") as f:
-            json.dump(metrics, f, indent=4)
-            
-        
-        # Log parameters and metrics using the MLflow APIs
-        # mlflow.set_tracking_uri("http://127.0.0.1:8080")
-        # signature = infer_signature(X_test_scaled, prediction)
-        # best_params = joblib.load("models/best_params.pkl")
-        # mlflow.log_params(best_params)
-        # mlflow.log_metrics(metrics)
-
-        # Log the sklearn model and register as version 1
-        mlflow.sklearn.log_model(
-            sk_model=my_model,
-            artifact_path="silica_concentrate",
-            signature=signature,
-            registered_model_name="silica_concentrate_model_V1",
-        )
+    # Sauvegarder les métriques dans un fichier JSON
+    with open(output_metrics + "/scores.json", "w") as f:
+        json.dump(metrics, f, indent=4)
 
 
 
